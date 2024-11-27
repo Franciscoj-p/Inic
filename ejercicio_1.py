7548f2f5ff6ddb37c133bfa0357ffae73d725f57
Franciscoj-p <jotafrans12@gmail.com>
2024-11-26 20:00:15 -0500

    uso de la funcion round()

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -10,15 +10,13 @@ for i in range(cantidad_materias):
     calificacion = float(input(f"Ingresa la calificación de {nombre_materia}: "))
     notas[nombre_materia] = calificacion  # Agregar al diccionario nombre_materia es la key y calificacion el valor
     
-promedio = sum(notas.values()) / len(notas)  #.values devuelve todos los valores
-
-if promedio >= 6.0:
-    print(f'Felicidades {nombre}, Aprobaste con un promedio de {promedio:.1f}') #:.1f limita los decimales a 1
+promedio = round(sum(notas.values()) / len(notas), 1)  #.values devuelve todos los valores
+minimo_aprobar = 6.0
+if promedio >= minimo_aprobar:
+    print(f'Felicidades {nombre}, Aprobaste con un promedio de {promedio}') 
 else:
-    print(f'Lo siento mucho {nombre}, No aprobaste, tienes un promedio de {promedio:.1f}')
+    print(f'Lo siento mucho {nombre}, No aprobaste, tienes un promedio de {promedio}, el minimo para pasar es {minimo_aprobar}')
     
     print("\nCalificaciones ingresadas:")
 for materia, calificacion in notas.items(): #almacena en materia, calificacion los pares ordenados que devuelve .items desde el diccionario notas
     print(f"{materia}: {calificacion}")
-
-print(f"\nEl promedio de tus notas es: {promedio:.2f}")
